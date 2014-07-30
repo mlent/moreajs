7df54f0cabf4ced43202d0542db70ab44362e0e2
Monica <lent.monica@gmail.com>
2014-07-30 10:44:47 -0500

    Each sentence in sep. obj. per lang.

--- demo/main.js
+++ demo/main.js
@@ -18,12 +18,33 @@ require(['morea'], function(Morea) {
 		lang: 'eng',
 		callback: function(response_text) {
 			var data = JSON.parse(response_text);
+			this.translations = Object.keys(data.translations); 
 
 			// Create array of sentence objects
-			this.translations = data.translations;
-
 			var sentences = [];
 			sentences[0] = data.words;
+			
+			for (var i = 0; i < sentences[0].length; i++) {
+				var word = sentences[0][i];
+
+				for (var j = 0; j < this.translations.length; j++) {
+					var lang = this.translations[j];
+
+					// HACK, 'til we have the lang attr avail on words
+					var words = word.translations.filter(function(trans) {
+						trans.lang = lang;					
+						return trans.CTS.indexOf(lang) !== -1;
+					});
+
+					if (sentences[j+1] === undefined) { 
+						sentences[j+1] = [];
+					}
+
+					sentences[j+1] = sentences[j+1].concat(words); 
+
+				}
+
+			}
 
 			return sentences;
 		}
