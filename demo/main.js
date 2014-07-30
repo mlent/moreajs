fa011a5966c741a293ed991338f3a7e4e5a38903
Monica <lent.monica@gmail.com>
2014-07-30 09:49:00 -0500

    Fetches data, allows user to specify callback, fixed grunt process.

--- demo/main.js
+++ demo/main.js
@@ -12,6 +12,21 @@ requirejs.config({
 
 require(['morea'], function(Morea) {
 
-	new Morea('div', { });
+	new Morea('div', { 
+		mode: 'edit',
+		dataUrl: 'data.json',
+		lang: 'eng',
+		callback: function(response_text) {
+			var data = JSON.parse(response_text);
+
+			// Create array of sentence objects
+			this.translations = data.translations;
+
+			var sentences = [];
+			sentences[0] = data.words;
+
+			return sentences;
+		}
+	});
 
 });
--- src/morea.js
+++ src/morea.js
@@ -27,15 +27,82 @@ define(function() {
 
 	};
 
+	morea.prototype.defaults = {
+		mode: 'edit',
+		data: null,					// Array of sentence objects
+		dataUrl: null,				// Or an endpoint + callback to process the data
+		orientation: 'horizontal'
+	};
+
 	/**
 	 * Initialize plugin.
 	 */
 	morea.prototype.init = function() {
+		this.config = this._extend({}, this.defaults, this.options);
+		this.data = this.config.data || this._fetchData(this.config.dataUrl);
+		this.render();
+	};
 
+	morea.prototype._fetchData = function(dataUrl) {
+		var request = new XMLHttpRequest();
+		var out = {};
+		request.open('GET', dataUrl, false);
+
+		request.onload = function() {
+			if (request.status >= 200 && request.status < 400) {
+				// Use the user's defined callback
+				out = this.config.callback(request.responseText);
+			}
+			else
+				console.log("");
+		}.bind(this);
+
+		request.send();
+		
+		return out;
 	};
 
 	morea.prototype.render = function(e) {
+		
+		if (this.el.className.indexOf('morea') === -1)
+			this.el.className += ' morea';
 
+		this.el.innerHTML = '';
+
+		// For each sentence, add words inside a subcontainer
+		for (var i = 0; i < this.data.length; i++) {
+			var el = document.createElement('div');
+			el.className = 'sentence';
+
+			for (var j = 0; j < this.data[i].length; j++) {
+				var word = document.createElement('span');
+				word.innerHTML = this.data[i][j].value;
+				el.appendChild(word);
+				el.appendChild(document.createTextNode(' '));
+			}
+
+			this.el.appendChild(el);
+		}
+
+	};
+
+	// ------------------------- //
+	// Utility Functions         //
+	// ------------------------- //
+	morea.prototype._extend = function(out) {
+		out = out || {};
+
+		for (var i = 1; i < arguments.length; i++) {
+			if (!arguments[i])
+				continue;
+
+			for (var key in arguments[i]) {
+				if (arguments[i].hasOwnProperty(key))
+					out[key] = arguments[i][key];
+			}
+		}
+
+		return out;
 	};
 
 	return morea;
